4f3e3244c2c918cb857ed9bdb7b90b59b7a9c26f
Michel Blanc <mb@mbnet.fr>
2014-03-21 16:48:03 +0100

    Fixes nfc poller
    Added signal handler
    Added command line delay option

--- griotte/tools/nfc/iso14443a-poller.c
+++ griotte/tools/nfc/iso14443a-poller.c
@@ -3,19 +3,23 @@
  */
 
 // To compile :
-// $ gcc -o quick_start_example1 quick_start_example1.c -lnfc
+// $ gcc -o iso14443a-poller iso14443a-poller.c -lnfc
 
 #include <stdlib.h>
 #include <nfc/nfc.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 nfc_device *pnd;
 // Allocate only a pointer to nfc_context
 nfc_context *context;
 
-void sigint_handler(int) {
-  // Close NFC device
+void sigint_handler(int whatever) {
+  printf("SIGINT trapped");
+
+  // Abort & close NFC device
+  nfc_abort_command(pnd);
   nfc_close(pnd);
 
   // Release the context
@@ -35,7 +39,14 @@ print_hex(const uint8_t *pbtData, const size_t szBytes)
 }
 
 static void
-poll_tags()
+print_usage(const char *progname)
+{
+  printf("usage: %s [-d x]\n", progname);
+  printf("  -d x\t wait x msecs after each tag detection\n");
+}
+
+static void
+poll_tags(int delay)
 {
 // Poll for a ISO14443A (MIFARE) tag
   const nfc_modulation nmMifare = {
@@ -47,18 +58,8 @@ poll_tags()
 
   do {
     if (nfc_initiator_select_passive_target(pnd, nmMifare, NULL, 0, &nt) > 0) {
-      // printf("The following (NFC) ISO14443A tag was found:\n");
-      // printf("    ATQA (SENS_RES): ");
-      // print_hex(nt.nti.nai.abtAtqa, 2);
-      // printf("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
       print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
-      return
-      // printf("      SAK (SEL_RES): ");
-      // print_hex(&nt.nti.nai.btSak, 1);
-      // if (nt.nti.nai.szAtsLen) {
-      //   printf("          ATS (ATR): ");
-      //   print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
-      // }
+      usleep(delay);
     }
   } while (true);
 }
@@ -66,9 +67,19 @@ poll_tags()
 int
 main(int argc, const char *argv[])
 {
+  int delay = 100;
   struct sigaction act;
   act.sa_handler = sigint_handler;
   sigaction(SIGINT, &act, NULL);
+
+  if (argc != 1) {
+    if ((argc == 3) && (0 == strcmp("-d", argv[1]))) {
+      delay = atoi(argv[2]);
+    } else {
+      print_usage(argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
 
   // Initialize libnfc and set the nfc_context
   nfc_init(&context);
@@ -79,7 +90,6 @@ main(int argc, const char *argv[])
 
   // Display libnfc version
   const char *acLibnfcVersion = nfc_version();
-  (void)argc;
   printf("%s uses libnfc %s\n", argv[0], acLibnfcVersion);
 
   // Open, using the first available NFC device which can be in order of selection:
@@ -101,5 +111,5 @@ main(int argc, const char *argv[])
 
   printf("NFC reader: %s opened\n", nfc_device_get_name(pnd));
 
-  poll_tags();
+  poll_tags(delay*1000);
 }
