0e3f117ee0d2d7559ecee7d47146205290701b58
Alain <hello@alainbarthelemy.com>
2015-02-06 11:58:30 +0100

    first

--- static/admin/js/blockly/generators/python/monkey_patching.js
+++
@@ -1,8 +0,0 @@
-goog.require('Blockly.Python');
-
-Blockly.Python['controls_repeat_forever'] = function(block) {
-  var branch = Blockly.Python.statementToCode(block, 'DO') || '  pass\n';
-
-  var code = 'while (True):\n' + branch;
-  return code;
-};
--- static/admin/js/blockly/msg/fr_monkey_patching.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-goog.provide('Blockly.Msg.fr');
-
-goog.require('Blockly.Msg');
-
-Blockly.Msg.DISCARD_CODE = "Supprimer tous les blocs ?"
-Blockly.Msg.OPEN_SCENARIO = "Ouvrir..."
-
